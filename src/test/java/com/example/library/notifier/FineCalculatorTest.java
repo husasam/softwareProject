39ea377c622c3c84fd752e0913e9ddabb36e6916
husamdarwazeh <husamnazeeh510@gmail.com>
2025-12-11 01:03:52 +0200

    rrrrrrrrr

--- src/test/java/com/example/library/notifier/FineCalculatorTest.java
+++ src/test/java/com/example/library/notifier/FineCalculatorTest.java
@@ -12,27 +12,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FineCalculatorTest {
 
-    @Test
-    void testBookFine() {
-        FineStrategy bookStrategy = new FineStrategy() {
-            @Override
-            public double calculateFine(int overdueDays) {
-                return overdueDays * 1.0;
-            }
-        };
-        FineStrategy cdStrategy = new FineStrategy() {
-            @Override
-            public double calculateFine(int overdueDays) {
-                return overdueDays * 2.0;
-            }
-        };
+	@Test
+	void testBookFine() {
+	    FineStrategy bookStrategy = overdueDays -> overdueDays * 1.0;
+	    FineStrategy cdStrategy = overdueDays -> overdueDays * 2.0;
 
-        FineCalculator calculator = new FineCalculator(bookStrategy, cdStrategy);
+	    FineCalculator calculator = new FineCalculator(bookStrategy, cdStrategy);
 
-        Book book = new Book("Title", "123", "Author");
-        BorrowRecord record = new BorrowRecord("user1", book, LocalDate.now().minusDays(3));
+	    Book book = new Book("Title", "123", "Author");
 
-        double fine = calculator.calculate(record, LocalDate.now());
-        assertEquals(3.0, fine, 0.01);
-    }
+	    BorrowRecord record = new BorrowRecord("user1", book, LocalDate.now().minusDays(3));
+
+	    record.setDueDate(LocalDate.now().minusDays(3));
+
+	    double fine = calculator.calculate(record, LocalDate.now());
+	    assertEquals(3.0, fine, 0.01);
+	}
+
 }
